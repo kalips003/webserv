748d3dea32dc59c65e66a9f2efe426fc77d260ee
umosse <ulysse.mosse@yahoo.fr>
2026-01-30 15:30:33 +0100

    WORKING - Commented signals function, replaced cout by write

--- src/class/Signals/Signal.cpp
+++ src/class/Signals/Signal.cpp
@@ -3,15 +3,37 @@
 #include "_colors.h"
 #include "Server.hpp"
 
+///////////////////////////////////////////////////////////////////////////////]
+/** @brief Signal handler for SIGINT (Ctrl+C)
+ *
+ * This function is called automatically when the program receives the SIGINT signal
+ * (typically triggered by pressing Ctrl+C in the terminal). Its main purpose is to
+ * perform a graceful shutdown of the server.
+ *
+ * @param signum The signal number (ignored in this handler).
+ * @param info   Additional signal information (ignored in this handler).
+ * @param context Processor context at the time of signal (ignored in this handler).
+ */
+
 static void handle_sigint(int signum, siginfo_t *info, void *context)
 {
 	(void)info;
 	(void)context;
 	(void)signum;
 	g_ServerEnd = true;
-	std::cout << RED << "\nSay goodbye to your cats buddy\n" << RESET;
+	write( 1, RED "\nSay goodbye to your cats buddy\n" RESET, 44);
 }
 
+///////////////////////////////////////////////////////////////////////////////]
+/* Initializes signal handling for the program.
+ *
+ * Registers a custom handler for SIGINT (Ctrl+C) using sigaction.
+ * The handler allows the server to shut down gracefully instead of
+ * terminating immediately when an interrupt signal is received.
+ *
+ * Returns:
+ *  0 on success
+ *  1 on failure */
 int init_signals(void)
 {
 	struct sigaction sig;
